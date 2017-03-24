42d5f71c295b9ed0cf78e1573ef64993cdc85bb0
Zhiyuan Wan <rgwan@rocaloid.org>
2017-03-24 09:37:38 +0800

    prepare: bus matrix

---
+++ rtl/urv_busmatrix.v
@@ -0,0 +1,19 @@
+/* 
+
+	Kamikaze-uRV: a tiny RISC-V RV32IMC core
+	
+	Copyright (c) 2017 Anlogic Technology
+	Author: Zhiyuan Wan <h@iloli.bid>
+	
+	This file realizes a bus matrix which provides following port:
+	
+	uRV Instruction Port (S, simple bus),
+	uRV Data Port (S, simple bus),
+	
+	uRV Debug and private peripheral port (M, simplebus),
+	uRV Debugger entry port(S, simplebus)
+	
+	Kamikaze I-Port (M, AHB-Lite),
+	Kamikaze D-Port (M, AHB-Lite),
+	Kamikaze S-Port (M, AHB-Lite)
+*/
